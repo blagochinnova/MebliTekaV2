5a9066f036cfe284e559a89082bc543fcb7e076d
Diiiii <diblagochinnova@gmail.com>
2025-07-25 23:08:56 +0300

    delete pattern for form

--- src/components/ConsultationModal.jsx
+++ src/components/ConsultationModal.jsx
@@ -97,7 +97,6 @@ export default function ConsultationModal({ isOpen, onClose }) {
                 onChange={handleChange}
                 required
                 placeholder="+38 (XXX) XXX-XX-XX"
-                pattern="\+38\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}"
                 title="Формат: +38 (XXX) XXX-XX-XX"
               />
               <label htmlFor="email">Email:</label>
